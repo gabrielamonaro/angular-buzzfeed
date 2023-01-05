83cda684579d79d9379272856ed14962364b1d5d
gabrielamonaro <gabriela.monaro@aluno.ufabc.edu.brgit config --global user.email gabriela.monaro@aluno.ufabc.edu.br>
2023-01-05 17:32:30 -0300

    Adicionando mecanismo de soma de opções e contagem da opção em moda

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
+  title:string =''
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string = ''
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+   finished:boolean = true
 
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions)
+    {
+      this.finished = false;
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+
+    }
+  }
+
+  playerChoice(value: string)
+  {
+    this.answers.push(value)
+    console.log(this.answers)
+    this.nextStep()
+
+  }
+
+  nextStep(){
+    this.questionIndex+=1
+    if (this.questionIndex == this.questionMaxIndex)
+    {
+      this.finished = true
+    }
+    else{
+      const finalAnswer: string=  this.checkResult(this.answers)
+      this.questionSelected = this.questions[this.questionIndex]
+      this.answerSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results]
+    }
+  }
+
+  checkResult(answers:string[])
+  {
+    const result = answers.reduce((previous, current, index, array) => {
+      if(
+        array.filter(item => item === previous).length > 
+        array.filter(item => item === current).length
+      ){
+        return previous
+      }
+      else{
+        return current
+      }
+    })
+    return result
+
   }
 
 }
